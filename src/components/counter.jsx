97a4e786b172b20bf2a322802901f61c7706f59e
mtw2017 <jilufe@gmail.com>
2018-12-09 15:05:06 +1300

    change

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,12 +4,15 @@ class Counter extends Component {
     render() {
 
         return (
-            <div>
-                <span className={getBadgeClasses.call(this)}>{this.formatCount()}</span>
-                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm m-2">+</button>
-                <button onClick={() => this.props.onDecrease(this.props.counter)} className="btn btn-secondary btn-sm" m-2>-</button>
-                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
-            </div>
+            <React.Fragment>
+                <div className="row">
+                    <div className="col-1"><span className={getBadgeClasses.call(this)}>{this.formatCount()}</span></div>
+                    <div className="col">
+                        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">+</button>
+                        <button onClick={() => this.props.onDecrease(this.props.counter)} disabled={!(this.props.counter.value>0)} className="btn btn-secondary btn-sm m-2">-</button>
+                        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm">x</button>
+                    </div>
+</div>      </React.Fragment>
         );
 
         function getBadgeClasses() {
@@ -18,7 +21,6 @@ class Counter extends Component {
             return classes;
         }
     }
-
 
     formatCount() {
         const {value} = this.props.counter;
